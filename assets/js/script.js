6e1d8aaa7a4982c2d0aa1ae2b3350a3b952b3c54
pricilliaedou <pricillia.edou@gmail.com>
2025-08-22 23:46:20 +0200

    animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+//Intersection Observer
+const observer = new IntersectionObserver((entries) => {
+  for (const entry of entries) {
+    if (entry.isIntersecting) {
+      entry.target.animate(
+        [
+          { transform: "translateX(-100px)", opacity: 0 },
+          { transform: "translateX(0)", opacity: 1 },
+        ],
+        { duration: 500, fill: "forwards" }
+      );
+      // observer.unobserve(entry.target);
+    }
+  }
+});
+observer.observe(document.querySelector("#myjoulebox"));
+observer.observe(document.querySelector("#demo"));
+observer.observe(document.querySelector("#techno"));
+// observer.disconnect();
+
+//Diapo
 let swiper = new Swiper(".swiper", {
   effect: "coverflow",
   grabCursor: true,
